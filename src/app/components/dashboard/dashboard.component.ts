da65225d9320a6b5f3b59c60d7352d6524f45e24
Javier <schjavier@gmail.com>
2025-02-02 11:39:52 -0300

    (fix) unused import removed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,9 @@
-import {Component, EventEmitter, Input, input, Output} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButton} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-import {ClientFormsComponent} from '../forms/client-forms/client-forms.component';
-import {ClientDashboardComponent} from './client-dashboard/client-dashboard.component';
-import {NgIf} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 @Component({
@@ -16,7 +13,6 @@ import {FormsModule} from '@angular/forms';
     MatButton,
     MatFormFieldModule,
     MatSelectModule,
-    ClientDashboardComponent,
     FormsModule,
     RouterLink,
     RouterOutlet,
