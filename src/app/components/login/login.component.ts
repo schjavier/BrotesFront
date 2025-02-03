327b2145f70fa36e1ed4f57481539b05988eed82
Javier <schjavier@gmail.com>
2025-02-03 07:52:46 -0300

    (add) styles to login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {RouterOutlet} from '@angular/router';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButton,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
 
   username = new FormControl('');
   password = new FormControl('');
+
 }
