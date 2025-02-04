5ec58d404fdd807d65fcbfe3f21a1f447ca74198
Javier <schjavier@gmail.com>
2025-02-04 13:18:33 -0300

    (add) clientCardComponent to render the clients information

--- src/app/components/dashboard/client-dashboard/client-list/client-list.component.ts
+++ src/app/components/dashboard/client-dashboard/client-list/client-list.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, output, Output} from '@angular/core';
 import {ClientService} from '../../../../services/client-service.service';
 import {Observable} from 'rxjs';
 import {Client} from '../../../../model/client/client';
-import {AsyncPipe} from '@angular/common';
+import {AsyncPipe, NgForOf, NgStyle} from '@angular/common';
+import {ClientCardComponent} from '../client-card/client-card.component';
 
 @Component({
   selector: 'app-client-list',
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    ClientCardComponent,
+    NgForOf,
+    NgStyle
   ],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.css'
 })
 export class ClientListComponent {
 
-  client$! : Observable<Client>;
+  clientList$!: Observable<Client[]>;
+  clients:Client[] = [];
+  id!:number;
 
   constructor(private clientService: ClientService) {
   }
 
-  ngOnInit():void {
-    this.client$ = this.clientService.getClientById(6);
+  ngOnInit() {
+    this.clientList$ = this.clientService.getAllClients();
+
+    this.clientList$.subscribe(clients => this.clients = clients);
+    console.log(this.clients);
+
   }
-
-
 }
