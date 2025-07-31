4156a352246f5a4dfca41406d3fb09a89fbe1922
Javier <schjavier@gmail.com>
2025-07-31 20:27:05 -0300

    fix: styles for create order component

--- src/app/components/forms/order/create-order-form/create-order-form.component.ts
+++ src/app/components/forms/order/create-order-form/create-order-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton, MatIconButton} from '@angular/material/button';
-import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatError, MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatOption} from '@angular/material/core';
 import {MatSelect} from '@angular/material/select';
@@ -20,7 +20,7 @@ import {Product} from '../../../../model/product/product';
 import {ItemPedidoDetailsDto} from '../../../../model/item-pedido/item-pedido-details-dto';
 import {CreateOrderDto} from '../../../../model/pedido/create-order-dto';
 import {MatIcon} from '@angular/material/icon';
-import {MatChipListbox, MatChipRow} from '@angular/material/chips';
+import {MatChipListbox, MatChipRemove, MatChipRow} from '@angular/material/chips';
 
 @Component({
   selector: 'app-create-order-form',
@@ -39,7 +39,9 @@ import {MatChipListbox, MatChipRow} from '@angular/material/chips';
         NgIf,
         MatChipListbox,
         MatChipRow,
-        MatError
+        MatError,
+        MatSuffix,
+        MatChipRemove
     ],
   templateUrl: './create-order-form.component.html',
   styleUrl: './create-order-form.component.css'
