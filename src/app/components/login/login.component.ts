f3ce6200b3e3ea9274b22e3943ddb9ace0f520d4
Javier <schjavier@gmail.com>
2025-08-18 07:01:53 -0300

    add: styles for some components, and completely change style form login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    ReactiveFormsModule,
-    MatButton,
-  ],
+    imports: [
+        ReactiveFormsModule,
+        MatButton,
+        MatLabel,
+        MatFormField,
+        MatInput,
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
 
-  username = new FormControl('');
-  password = new FormControl('');
+    macroTitle:string = "Bienvenido al sistema de gestión de pedidos de"
+    title:string = "Brotes"
+    subtitle:string = "Organicos"
+
+    authForm: FormGroup = new FormGroup({
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+    })
+
+
 
 }
