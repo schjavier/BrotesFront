57de173ade028539198465eb313c8e6dbd4b575b
Javier <schjavier@gmail.com>
2025-08-18 08:09:25 -0300

    fix: change styles from all components to ensure style consistency

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {MatButton} from '@angular/material/button';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ import {MatInput} from '@angular/material/input';
         MatLabel,
         MatFormField,
         MatInput,
+        NgOptimizedImage,
     ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
